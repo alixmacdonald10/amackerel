78b63079b088f7973a50c4588469acd32c848c88
amackerel <amackerel@proton.me>
2026-09-02 22:14:56 +0100

    refactor: footer css refactor to highlight

--- src/components/footer.rs
+++ src/components/footer.rs
@@ -1,7 +1,23 @@
 use topcoat::{
-    view::{component, view},
+    view::{class, component, view, StaticClass},
     Result,
 };
+
+const VERSION_CSS: StaticClass = class!(
+    "text-xs",
+    "tracking-tight",
+    "text-foreground",
+    "transition",
+    "delay-50",
+    "duration-200",
+    "ease-in-out",
+    "hover:text-primary",
+    "hover:-translate-y-1",
+    "hover:scale-110",
+    "focus-visible:outline-2",
+    "focus-visible:outline-offset-2",
+    "focus-visible:outline-ring"
+);
 
 #[component]
 pub async fn footer() -> Result {
@@ -9,9 +25,7 @@ pub async fn footer() -> Result {
 
     view! {
         <footer class="fixed bottom-3 right-4 z-50">
-            <p
-                class="text-xs tracking-tight text-foreground hover:text-primary"
-            >
+            <p class=(VERSION_CSS)>
                 (version)
             </p>
         </footer>
